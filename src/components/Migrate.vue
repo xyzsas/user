be617ba8b401d097b4de4433c47c7a94b1b3c24d
CutePikachu <z5191511@ad.unsw.edu.au>
2020-09-19 09:51:02 +1000

    add migrate user and fix group display issue

---
+++ src/components/Migrate.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="add">
+    <h2 style="margin: 20px;">
+      待更新用户({{ users.length }})
+      <v-btn style="margin: 10px;" color="secondary" @click="sheet = true">导入用户信息</v-btn>
+      <v-btn style="margin: 10px;" :loading="loading" color="primary" @click="upload">上传用户信息</v-btn>
+    </h2>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      class="elevation-1"
+    ></v-data-table>
+    <v-bottom-sheet v-model="sheet" inset>
+      <v-sheet style="padding: 30px 20px;">
+        <h3>导入用户数据</h3>
+        <p>请将用户数据粘贴到文本框中并点击确认（每行一个用户，数据之间用空格或tab隔开）。</p>
+        <v-textarea height="40vh" outlined label="用户名 新用户组" v-model="raw"></v-textarea>
+        <v-btn color="secondary" @click="importRaw">确认</v-btn>
+      </v-sheet>
+    </v-bottom-sheet>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title>用户管理</v-card-title>
+        <v-card-text>{{ message }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false">确定</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+const SS = window.sessionStorage
+import Hashes from 'jshashes'
+
+function hash1 (msg) {
+  return new Hashes.MD5().b64(msg).substr(7, 10)
+}
+
+export default {
+  name: 'Add',
+  data: () => ({
+    sheet: false,
+    dialog: false,
+    message: '',
+    loading: false,
+    raw: '',
+    headers: [
+      { text: '用户名', value: 'id' },
+      { text: '用户组', value: 'group' },
+      { text: '状态', value: 'status' }
+    ],
+    users: []
+  }),
+  methods: {
+    importRaw () {
+      const t = this.raw.split('\n')
+      for (let u of t) {
+        const c = u.split(/\s+/)
+        if (c.length < 2) continue
+        this.users.push({
+          id: c[0],
+          group: c[1]
+        })
+      }
+      this.raw = ''
+      this.sheet = false
+    },
+    async upload () {
+      this.loading = true
+      for (let i = 0; i < this.users.length; i++) {
+        const u = this.users[i]
+        const res = await this.$ajax.put('/admin/user?id=' + encodeURIComponent(hash1(u.id.toUpperCase())), { group: u.group }, {
+          headers: { token: SS.token }
+        })
+          .then( () => {
+            this.users.splice(i, 1)
+            i--
+            return true
+          })
+          .catch(err => {
+            this.users[i].status = '错误！'
+            this.message = err.response.data
+            this.dialog = true
+            return false
+          })
+        if (!res) break
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div.add {
+    padding: 30px 50px;
+  }
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,6 +3,7 @@
     <v-tabs v-model="tab">
       <v-tab key="overview">概览</v-tab>
       <v-tab key="add">添加用户</v-tab>
+      <v-tab key="migrate">迁移用户</v-tab>
       <v-tab key="user">查询用户</v-tab>
       <v-tab key="group">用户组</v-tab>
     </v-tabs>
@@ -13,6 +14,9 @@
       </v-tab-item>
       <v-tab-item key="add">
         <add></add>
+      </v-tab-item>
+      <v-tab-item key="migrate">
+        <migrate></migrate>
       </v-tab-item>
       <v-tab-item key="user">
         <user :uid="uid" :random="random"></user>
@@ -30,6 +34,7 @@ import Overview from '@/components/Overview.vue'
 import Add from '@/components/Add.vue'
 import User from '@/components/User.vue'
 import Group from '@/components/Group.vue'
+import Migrate from '@/components/Migrate.vue'
 
 export default {
   name: 'Admin',
@@ -39,14 +44,14 @@ export default {
     random: 0
   }),
   components: {
-    Overview, Add, User, Group
+    Overview, Add, User, Group, Migrate
   },
   mounted () {
     if (SS.role !== 'ADMIN') window.location.href = '/index.html'
   },
   methods: {
     user (uid) {
-      this.tab = 2
+      this.tab = 3
       this.uid = uid
       this.random++
     }
